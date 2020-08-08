5c7a8bdfaf2a693bb30c80cf679b8587ad2606f8
iLoreto <iloreto@debian1.5tkodnfwvigufko0zrg5u302tf.xx.internal.cloudapp.net>
2020-08-07 18:28:43 -0700

    moved download part into single .sh

--- trans/deps/build_trans.sh
+++ trans/deps/build_trans.sh
@@ -1,36 +1,32 @@
-wget http://monkey.org/~provos/libevent-1.4.11-stable.tar.gz
+./download_deps.sh
 tar xvfz libevent-1.4.11-stable.tar.gz
 cd libevent-1.4.11-stable
 ./configure --prefix=/var/tools/trans/build
 make
 make install
-rm -rf lib*
+rm -rf libevent-1.4.11-stable/
 cd ..
-#wget http://zlib.net/zlib-1.2.11.tar.gz
 tar xvfz zlib-1.2.11.tar.gz
 cd zlib-1.2.11
 ./configure --prefix=/var/tools/trans/build
 make
 make install
-rm -rf zlib*
+rm -rf zlib-1.2.11/
 cd ..
-#wget http://openssl.org/source/openssl-1.0.2d.tar.gz
 tar xvfz openssl-1.0.2d.tar.gz
 cd openssl-1.0.2d
 ./config --prefix="/var/tools/trans/build" shared zlib-dynamic -I"/var/tools/trans/build/include" 
 make
 make install
-rm -rf open*
+rm -rf openssl-1.0.2d/
 cd ..
-#wget http://curl.haxx.se/download/curl-7.71.1.tar.gz
 tar xvfz curl-7.71.1.tar.gz
 cd curl-7.71.1
 ./configure --prefix="/var/tools/trans/build" --with-zlib="/var/tools/trans/build/" --with-ssl="/var/tools/trans/build"
 make
 make install
-rm -rf curl*
+rm -rf curl-1.71.1/
 cd ..
-#wget https://github.com/transmission/transmission-releases/raw/master/transmission-2.03.tar.bz2
 tar xjf transmission-2.03.tar.bz2
 cd transmission-2.03
 ./configure --prefix=/var/tools/trans/build/transmission \
@@ -43,5 +39,5 @@ cd transmission-2.03
  	CPPFLAGS=-DTR_EMBEDDED
 make
 make install
-rm -rf trans*
+rm -rf transmission-2.03/
 cd ..
---
+++ trans/deps/download_deps.sh
@@ -0,0 +1,5 @@
+wget http://monkey.org/~provos/libevent-1.4.11-stable.tar.gz
+wget http://zlib.net/zlib-1.2.11.tar.gz
+wget http://openssl.org/source/openssl-1.0.2d.tar.gz
+wget http://curl.haxx.se/download/curl-7.71.1.tar.gz
+wget https://github.com/transmission/transmission-releases/raw/master/transmission-2.03.tar.bz2
